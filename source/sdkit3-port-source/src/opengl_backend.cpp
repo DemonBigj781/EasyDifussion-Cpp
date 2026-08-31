9a10db0c46d74c1c9a4647b9906f38944b266052
DemonBigj781 <20097331+DemonBigj781@users.noreply.github.com>
2026-08-31 10:06:56 -0400

    Add experimental OpenGL backend compile scaffold

---
+++ source/sdkit3-port-source/src/opengl_backend.cpp
@@ -0,0 +1,38 @@
+#include "opengl_backend.h"
+
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
+#include <GL/gl.h>
+
+namespace sdkit::opengl {
+
+bool compiled() {
+    return true;
+}
+
+std::string implementation_name() {
+    return "OpenGL/GLSL experimental backend";
+}
+
+std::string api_version() {
+#if defined(GL_VERSION_4_6)
+    return "OpenGL headers >= 4.6";
+#elif defined(GL_VERSION_4_5)
+    return "OpenGL headers >= 4.5";
+#elif defined(GL_VERSION_4_3)
+    return "OpenGL headers >= 4.3";
+#else
+    return "OpenGL headers available";
+#endif
+}
+
+bool compute_ready() {
+    // The first Theory milestone is compile/link plumbing only. This remains
+    // false until SSBO allocation, GLSL compute kernels and GGML registration
+    // are implemented.
+    return false;
+}
+
+} // namespace sdkit::opengl
