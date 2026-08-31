0b494684f3910cba629aea417bd91f118c0cfd82
DemonBigj781 <20097331+DemonBigj781@users.noreply.github.com>
2026-08-31 10:07:11 -0400

    Keep OpenGL scaffold isolated when backend is disabled

--- source/sdkit3-port-source/src/opengl_backend.cpp
+++ source/sdkit3-port-source/src/opengl_backend.cpp
@@ -1,15 +1,20 @@
 #include "opengl_backend.h"
 
-#if defined(_WIN32)
-#include <windows.h>
+#if defined(SDKIT_ENABLE_OPENGL_BACKEND)
+#  if defined(_WIN32)
+#    include <windows.h>
+#  endif
+#  include <GL/gl.h>
 #endif
-
-#include <GL/gl.h>
 
 namespace sdkit::opengl {
 
 bool compiled() {
+#if defined(SDKIT_ENABLE_OPENGL_BACKEND)
     return true;
+#else
+    return false;
+#endif
 }
 
 std::string implementation_name() {
@@ -17,14 +22,18 @@ std::string implementation_name() {
 }
 
 std::string api_version() {
-#if defined(GL_VERSION_4_6)
+#if defined(SDKIT_ENABLE_OPENGL_BACKEND)
+#  if defined(GL_VERSION_4_6)
     return "OpenGL headers >= 4.6";
-#elif defined(GL_VERSION_4_5)
+#  elif defined(GL_VERSION_4_5)
     return "OpenGL headers >= 4.5";
-#elif defined(GL_VERSION_4_3)
+#  elif defined(GL_VERSION_4_3)
     return "OpenGL headers >= 4.3";
-#else
+#  else
     return "OpenGL headers available";
+#  endif
+#else
+    return "OpenGL backend not compiled";
 #endif
 }
 
